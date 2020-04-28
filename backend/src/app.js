afec2b4678f97224680bc3031b176ada446de4a7
Victor França <63361486+victorfclima@users.noreply.github.com>
2020-04-28 20:08:41 -0300

    Back-end application and tests

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const { errors } = require('celebrate')
 const routes = require('./routes')
 
 const app = express()
@@ -8,7 +9,6 @@ app.use(cors())
 //Informando ao express que estaremos utilizando o JSON para o corpo das requisições
 app.use(express.json())
 app.use(routes)
+app.use(errors())
 
-
-
-app.listen(3333)
+module.exports = app
